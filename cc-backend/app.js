42f7d44b983a36b059a8d5c225eacf53cd4dc436
Ferdi761 <ferdiansyahachmad29@gmail.com>
2023-05-22 15:56:32 +0700

    add init db conn

--- cc-backend/app.js
+++ cc-backend/app.js
@@ -1,10 +1,29 @@
 const express = require('express')
-// import express from 'express'
 const app = express()
-require('dotenv').config()
+const { Sequelize } = require('sequelize')
+require('dotenv').config()  // for read env file
 const router = require('./router/routes')
-
 const port = process.env.PORT || 8000
+
+// db env
+const db = process.env.DB || 'antilergi'
+const username = process.env.USERNAME || 'admin'
+const password = process.env.PASSWORD || 'admin123'
+const dbHost = process.env.DBHOST || 'localhost'
+const dbDriver = process.env.DBDRIVER || 'mysql'
+
+// connect to DB orm
+const sequelize = new Sequelize('antilergi', 'user', 'password', {
+    host: 'dbHost',
+    dialect: 'dbDriver',
+  })
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 app.use('/', router)
 
