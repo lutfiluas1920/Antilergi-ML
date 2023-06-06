6087ccab21aed1a936f2ef9454c1ecafd0b8c412
Lutfi Aji Saputra <90809553+lutfiluas1920@users.noreply.github.com>
2023-06-07 01:10:56 +0800

    Delete cc-backend directory

--- cc-backend/routers/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const { helloWorld } = require('../controller/helloWorld')
-
-
-router.route('/').get(helloWorld)
-
-module.exports = router
